f144c28345d56df066f06af15bdf1e25f9a48a57
nicolaad <dalaaker.nicolai@gmail.com>
2018-10-30 16:10:37 +0100

    fixed footer icon, added temp about file to show footer

--- Web Technology/Project folder/js/footer.js
+++ Web Technology/Project folder/js/footer.js
@@ -22,18 +22,18 @@ function makeFooter(){
   contactInfo.innerHTML = "<a href=\"mailto:eksampleEmail@notTheActualEPRMEmail.com\" style=color:white>eprm@rvo.nl</a><br/>+31 703 798 000";
 
   //addding path to ..img/footerLogo.png image
-  logoImg.src = "../img/footerLogo.png";
+  logoImg.src = "img/footerLogo.png";
   //assignts style to footer
-  footer.style.cssText = "height: 150px; background-color:#C32323; color:white; font-family:\"Open Sans\"; position:relative; ";
+  footer.style.cssText = "position:fixed; bottom:10px; height: 150px; background-color:#C32323; color:white; font-family:\"Open Sans\"; position:relative; ";
 
   contactInfoHeader.style.cssText="opacity:0.33;";
   findUsHereHeader.style.cssText="opacity:0.33;";
   //asigns positions for the footerLeft footerRight divs in an horizontal manner
-  footerLeft.style.cssText="float: left; margin-left:10%";
-  footerRight.style.cssText="float:left; position:relative; left:10%;";
+  footerLeft.style.cssText="float:left; position:relative; margin-left:10%; z-index:2;";
+  footerRight.style.cssText="float:left; position:relative; left:10%; z-index:2;";
 
   //puts logoImg in the bottom right of the footer
-  logoImg.style.cssText="position:absolute; bottom:0; right:0; height:150px";
+  logoImg.style.cssText="position:absolute; bottom:0px; right:0px; height:150px; z-index:1; opacity:0.5;";
 
   //appends textelements to right and left footer div
   footerLeft.appendChild(findUsHereHeader);
