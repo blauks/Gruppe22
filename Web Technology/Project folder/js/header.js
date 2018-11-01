b14c8ff2d81a8b68ac6303f5baa4c4694fe366e4
blauks <brooks.jonathan1@gmail.com>
2018-11-01 17:59:46 +0100

    changed changes

--- Web Technology/Project folder/js/header.js
+++ Web Technology/Project folder/js/header.js
@@ -1,8 +1,18 @@
-//remember to add an empty div with ID=head at the top of the HTML document
+//remember to add an empty header with ID=head at the top of the HTML document
 
 function getInvolvedAnimation(){
     document.body.appendChild(transparentBackground);
+    clickBox1.style.margin = "0px 37.5%";
     transparentBackground.setAttribute("id", "transparentBackground");
+    let x = 1;
+    let moveBox = setInterval(moveBoxFunc, 10);
+    function moveBoxFunc(){
+        clickBoxDiv.style.margin = x + "px 37.5%";
+        x = x*1.1;
+        if(x > 195){
+            clearInterval(moveBox);
+        }
+    }
     }
 
 //For animation of the get involved button
@@ -13,6 +23,10 @@ const clickBoxDiv = document.createElement("div");
 clickBoxDiv.appendChild(clickBox1);
 clickBoxDiv.appendChild(clickBox2);
 transparentBackground.appendChild(clickBoxDiv);
+
+clickBox1.setAttribute("id","clickBox1");
+clickBox2.setAttribute("id","clickBox2");
+clickBoxDiv.setAttribute("id","clickBoxDiv")
 
 //For navbar
 const logo = document.createElement("img")
@@ -37,21 +51,21 @@ logo.setAttribute("id", "headerLogo");
 
 const buttonList = [about,projects,news,getInvolved]
 
-about.appendChild(document.createTextNode("ABOUT"))
-projects.appendChild(document.createTextNode("PROJECTS"))
-news.appendChild(document.createTextNode("NEWS"))
-getInvolved.appendChild(document.createTextNode("GET INVOLVED"))
+about.appendChild(document.createTextNode("ABOUT"));
+projects.appendChild(document.createTextNode("PROJECTS"));
+news.appendChild(document.createTextNode("NEWS"));
+getInvolved.appendChild(document.createTextNode("GET INVOLVED"));
 
-document.getElementById("head").appendChild(logo)
-document.getElementById("head").appendChild(about)
-document.getElementById("head").appendChild(projects)
-document.getElementById("head").appendChild(news)
-document.getElementById("head").appendChild(getInvolved)
+document.getElementById("head").appendChild(logo);
+document.getElementById("head").appendChild(about);
+document.getElementById("head").appendChild(projects);
+document.getElementById("head").appendChild(news);
+document.getElementById("head").appendChild(getInvolved);
 
 const buttons = document.createElement("div");
 buttons.setAttribute("id","allHeaderButtons");
-buttons.appendChild(about)
-buttons.appendChild(projects)
-buttons.appendChild(news)
-buttons.appendChild(getInvolved)
-document.getElementById("head").appendChild(buttons)
+buttons.appendChild(about);
+buttons.appendChild(projects);
+buttons.appendChild(news);
+buttons.appendChild(getInvolved);
+document.getElementById("head").appendChild(buttons);
