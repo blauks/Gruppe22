2577d8be0b04335de121474acdbb88a759330055
nicolaad <dalaaker.nicolai@gmail.com>
2018-11-06 17:51:00 +0100

    fighting chaos

--- Web Technology/Project folder/js/footer.js
+++ Web Technology/Project folder/js/footer.js
@@ -1,5 +1,3 @@
-
-
 //ads footer on run
 makeFooter();
 
@@ -24,7 +22,7 @@ function makeFooter(){
   //addding path to ..img/footerLogo.png image
   logoImg.src = "img/footerLogo.png";
   //assignts style to footer
-  footer.style.cssText = "bottom:10px; height: 150px; background-color:#C32323; color:white; font-family:\"Open Sans\"; position:relative; ";
+  footer.style.cssText = "position:fixed; bottom:10px; height: 150px; background-color:#C32323; color:white; font-family:\"Open Sans\"; position:relative; ";
 
   contactInfoHeader.style.cssText="opacity:0.33;";
   findUsHereHeader.style.cssText="opacity:0.33;";
