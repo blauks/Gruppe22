c128981af5f98c3697b25a81b84c5f762e3109aa
Javelja <30442901+Javelja@users.noreply.github.com>
2018-11-09 12:48:20 +0100

    final changes

--- Web Technology/Project folder/js/projects.js
+++ Web Technology/Project folder/js/projects.js
@@ -1,4 +1,6 @@
 var coll = document.getElementsByClassName("collapsible")
+var activeImages = document.getElementsByClassName("activeImage");
+var activeMaps = document.getElementsByClassName("activeMap");
 var i;
 
 for (i=0; i < coll.length; i++) {
@@ -10,5 +12,19 @@ for (i=0; i < coll.length; i++) {
 		} else {
 			content.style.display = "block";
 		}
+
+		for (i = 0; i < activeImages.length; i++) {
+			activeImages[i].classList.toggle("activeImage");
+		}
+		
+		for (i = 0; i < activeMaps.length; i++) {
+			activeMaps[i].classList.toggle("activeMap");
+		}
+		
+		var artImage = content.childNodes[1].innerHTML;
+		document.getElementById(artImage).classList.toggle("activeImage");
+		var artMap = content.childNodes[3].innerHTML;
+		document.getElementById(artMap).classList.toggle("activeMap");
+		
 	});
 }
